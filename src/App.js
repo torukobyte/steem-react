67992629ff09a9a324ad8026286a230dc1871c03
Burak KALAYCI <torukobyte@gmail.com>
2021-11-01 20:19:35 +0300

    steem

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import steem from "steem";
+import moment from "moment";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([]);
+    const [username, setUsername] = useState("ned");
+
+    useEffect(() => {
+        let isMounted = true
+        steem.api.getAccountHistory(username, -1, 100, (err, result) => {
+            let transfers = result.filter(tx => tx[1].op[0] === 'transfer')
+            let myArray = []
+            for (let item in transfers) {
+                myArray.push(transfers[item][1])
+            }
+            setData(myArray)
+        })
+        return () => {
+            isMounted = false
+        }
+    }, [username])
+
+    console.log(username)
+    return (
+        <div className="App">
+
+            <input value={username} type="text" onChange={(e) => setUsername(e.target.value)} />
+
+            {
+                data.map((dt, index) => (
+
+                    <p key={index}>{moment(dt.timestamp).fromNow()} - Claim rewards: {dt.op[1].amount}</p>
+                ))
+            }
+
+        </div>
+    );
 }
 
 export default App;
